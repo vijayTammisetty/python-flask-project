eb550701f9326402a714bbf5a1ff65017cbac40e
vijayTammisetty <120720424+vijayTammisetty@users.noreply.github.com>
2024-04-24 13:13:36 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mys3bucket-pythonflask" # Replace with your actual S3 bucket name
+    bucket = "mys3bucket-flask" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
